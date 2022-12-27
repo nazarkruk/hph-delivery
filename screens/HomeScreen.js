0c8f5182a351414a00fe8e4b9aff22e23db11751
Your NameNazar <nazarkruk@icloud.com>
2022-12-26 22:11:53 -0500

    Building App structure

---
+++ screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { SafeAreaView, ScrollView, View } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View></View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
